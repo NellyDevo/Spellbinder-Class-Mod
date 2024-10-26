8de0a79f9294522fbd19f45b395804d41acd3214
NellyDevo <Noelle.Devonshire@yahoo.com>
2024-10-26 15:54:58 -0400

    add enchanter's magic batteries

--- Mods/Spellbinder/ScriptExtender/Lua/Server/BoundSpells/BoundSpellDynamicDuration.lua
+++ Mods/Spellbinder/ScriptExtender/Lua/Server/BoundSpells/BoundSpellDynamicDuration.lua
@@ -1,16 +1,3 @@
-local function getBoundLifetime(entity)
-    if entity.Wielding then
-        entity = entity.Wielding.Owner
-    end
-    local duration
-    if Osi.HasPassive(entity.Uuid.EntityUuid, 'Spellbinder_Enchanter_Infinite_Bind_Duration') == 1 then
-        duration = -6
-    else
-        duration = math.max(1, math.floor((Osi.GetAbility(entity.Uuid.EntityUuid, "Intelligence") - 10) / 2) * 2) * 6
-    end
-    return duration
-end
-
 Ext.Osiris.RegisterListener("StatusApplied", 4, "after", function (object, status, causee, storyActionID)
     Ext.Timer.WaitFor(100, function ()
         local statusTag = "TAGBOUND"
@@ -20,7 +7,13 @@ Ext.Osiris.RegisterListener("StatusApplied", 4, "after", function (object, statu
         for statusEntity,statusID in pairs(entity.StatusContainer.Statuses) do
             if statusID == status then
                 if statusEntity.StatusLifetime.Lifetime < 0 then
-                    Osi.ApplyStatus(object, status, getBoundLifetime(entity), 1)
+                    if entity.Wielding then
+                        entity = entity.Wielding.Owner
+                    end
+                    if Osi.HasPassive(entity.Uuid.EntityUuid, 'Spellbinder_Enchanter_Magic_Batteries') == 0 then
+                        local duration = math.max(1, math.floor((Osi.GetAbility(entity.Uuid.EntityUuid, "Intelligence") - 10) / 2) * 2) * 6
+                        Osi.ApplyStatus(object, status, duration, 1)
+                    end
                 end
                 break
             end
