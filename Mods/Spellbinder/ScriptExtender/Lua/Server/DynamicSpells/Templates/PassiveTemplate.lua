71c70eab920f1bb886851b61a8477b94666b3aac
NellyDevo <Noelle.Devonshire@yahoo.com>
2024-11-06 10:46:30 -0500

    implement Magical Arc feature

--- Mods/Spellbinder/ScriptExtender/Lua/Server/DynamicSpells/Templates/PassiveTemplate.lua
+++ Mods/Spellbinder/ScriptExtender/Lua/Server/DynamicSpells/Templates/PassiveTemplate.lua
@@ -2,13 +2,17 @@ local __dynHelper = Ext.Require("Server/DynamicSpells/DynamicHelper.lua")
 
 local Passives = {}
 
-local function weaponPassive(spellID)
+local function weaponPassive(spellID, spellStat)
+    local magicalArc = ""
+    if spellStat.Level > 0 then
+        magicalArc = "IF(not HasStatus('SPELLBINDER_LOOP_STOPPER',context.Source) and HasPassive('Spellbinder_SpellDancer_MagicalArc_Marker', context.Source)):ApplyStatus(SELF,SPELLBINDER_SPELL_DANCER_MAGICAL_ARC,100,1);"
+    end
     return string.format(
 [[new entry "Spellbinder_Bound_Spell_%s"
 type "PassiveData"
 using "Spellbinder_Bound_Spell_BasePassive"
-data "StatsFunctors" "IF(not HasStatus('SPELLBINDER_LOOP_STOPPER',context.Source)):ApplyStatus(STATUS_TARGET_TRIGGER_%s);IF(not HasStatus('SPELLBINDER_LOOP_STOPPER',context.Source)):ApplyStatus(SELF,STATUS_CASTER_TRIGGER_%s,100,1);IF(not HasStatus('SPELLBINDER_LOOP_STOPPER',context.Source)):ApplyEquipmentStatus(SELF,GetAttackWeapon(),STATUS_SPELLBINDER_BOUND_SPELL_REMOVER,100,0);IF(not HasStatus('SPELLBINDER_LOOP_STOPPER',context.Source)):ApplyStatus(SELF,SPELLBINDER_LOOP_STOPPER)"]],
-    spellID, spellID, spellID)
+data "StatsFunctors" "IF(not HasStatus('SPELLBINDER_LOOP_STOPPER',context.Source)):ApplyStatus(STATUS_TARGET_TRIGGER_%s);IF(not HasStatus('SPELLBINDER_LOOP_STOPPER',context.Source)):ApplyStatus(SELF,STATUS_CASTER_TRIGGER_%s,100,1);IF(not HasStatus('SPELLBINDER_LOOP_STOPPER',context.Source)):ApplyEquipmentStatus(SELF,GetAttackWeapon(),STATUS_SPELLBINDER_BOUND_SPELL_REMOVER,100,0);%sIF(not HasStatus('SPELLBINDER_LOOP_STOPPER',context.Source)):ApplyStatus(SELF,SPELLBINDER_LOOP_STOPPER)"]],
+    spellID, spellID, spellID, magicalArc)
 end
 
 local function casterPassive(progress, spellID)
@@ -26,7 +30,7 @@ end
 ---@param spellStat SpellData
 ---@return string
 Passives.CreatePassive = function(spellID, spellStat)
-    local weapon = weaponPassive(spellID)
+    local weapon = weaponPassive(spellID, spellStat)
     return casterPassive(weapon, spellID)
 end
 
