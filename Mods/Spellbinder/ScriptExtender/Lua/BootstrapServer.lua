efc915dfca28a81ade588fe1c991424adb5d9034
NellyDevo <Noelle.Devonshire@yahoo.com>
2024-11-17 21:24:52 -0500

    rework battle dance

--- Mods/Spellbinder/ScriptExtender/Lua/BootstrapServer.lua
+++ Mods/Spellbinder/ScriptExtender/Lua/BootstrapServer.lua
@@ -1,13 +1,11 @@
-Ext.Require("Server/CharacterCreationWeapon.lua")
-Ext.Require("Server/WeaponMastery.lua")
-Ext.Require("Server/AbilityScalingFix.lua")
+local function requireAll(prefix, fileNameList)
+    for _,fileName in pairs(fileNameList) do
+        Ext.Require(prefix .. "/" .. fileName .. ".lua")
+    end
+end
 Ext.Require("Server/DynamicSpells/DynamicBindingSpells.lua")
-Ext.Require("Server/BoundSpells/BoundWeaponThrown.lua")
-Ext.Require("Server/BoundSpells/RemoveBoundSpellTag.lua")
-Ext.Require("Server/BoundSpells/UnlockBindSpells.lua")
-Ext.Require("Server/BoundSpells/RemoveRecentlyBoundStatus.lua")
-Ext.Require("Server/BoundSpells/ReweaveToBind.lua")
-Ext.Require("Server/SpectralArrows.lua")
+requireAll("Server/BoundSpells", {"BoundWeaponThrown","RemoveBoundSpellTag","UnlockBindSpells","RemoveRecentlyBoundStatus","ReweaveToBind"})
+requireAll("Server", {"CharacterCreationWeapon","WeaponMastery","AbilityScalingFix","SpectralArrows","BattleDance"})
 
 --Ext.Require("NuclearDumps.lua")
 
---
+++ Mods/Spellbinder/ScriptExtender/Lua/Server/BattleDance.lua
@@ -0,0 +1,23 @@
+Ext.Osiris.RegisterListener("StatusApplied", 4, "after", function (object, status, causee, storyActionID)
+    if status == "SPELLBINDER_SPELLDANCER_BATTLE_DANCE_APPLIER" then
+        if Osi.HasActiveStatus(object, "SPELLBINDER_SPELLDANCER_BATTLE_DANCE_TRACKER") == 0 then
+            local characterEntity = Ext.Entity.Get(object)
+            LazyExport("CharacterEntity", characterEntity)
+            local actionResource = characterEntity.ActionResources.Resources["d6b2369d-84f0-4ca4-a3a7-62d2d192a185"][1]
+            _D(actionResource)
+            local used = actionResource.MaxAmount - actionResource.Amount
+            Osi.ApplyStatus(object, "SPELLBINDER_SPELLDANCER_BATTLE_DANCE_TRACKER", used, 1)
+        end
+        Osi.ApplyStatus(object, "SPELLBINDER_SPELLDANCER_BATTLE_DANCE", 1)
+    elseif status == "SPELLBINDER_SPELLDANCER_BATTLE_DANCE_MOVE_TRIGGER" then
+        local characterEntity = Ext.Entity.Get(object)
+        local prevUsed = Osi.GetStatusCurrentLifetime(object, "SPELLBINDER_SPELLDANCER_BATTLE_DANCE_TRACKER")
+        local distanceAvailable = Osi.GetStatusTurns(object, "SPELLBINDER_SPELLDANCER_BATTLE_DANCE") * 3
+        local actionResource = characterEntity.ActionResources.Resources["d6b2369d-84f0-4ca4-a3a7-62d2d192a185"][1]
+        local used = actionResource.MaxAmount - actionResource.Amount
+        if used - prevUsed >= distanceAvailable then
+            Osi.RemoveStatus(object, "SPELLBINDER_SPELLDANCER_BATTLE_DANCE_TRACKER")
+            Osi.RemoveStatus(object, "SPELLBINDER_SPELLDANCER_BATTLE_DANCE")
+        end
+    end
+end)
